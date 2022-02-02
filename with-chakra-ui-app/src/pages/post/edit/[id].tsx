80ea7bfea07f3916b41f70cf6b2ca3899bf7a6ad
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-02-02 14:12:30 -0700

    edit post page update

--- with-chakra-ui-app/src/pages/post/edit/[id].tsx
+++ with-chakra-ui-app/src/pages/post/edit/[id].tsx
@@ -1,11 +1,78 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/dist/client/router";
+import { InputField } from "../../../components/InputField";
 import { Layout } from "../../../components/Layout";
-import { useUpdatePostMutation } from "../../../generated/graphql";
+import {
+    usePostQuery,
+    useUpdatePostMutation,
+} from "../../../generated/graphql";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
 
 const EditPost = ({}) => {
-    const [, editPost] = useUpdatePostMutation();
-    return <Layout></Layout>;
+    const [editPost] = useUpdatePostMutation();
+    const router = useRouter();
+    const intId =
+        typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+    const [{ data, fetching }] = usePostQuery({
+        pause: intId === -1,
+        variables: { id: intId },
+    });
+
+    if (fetching) {
+        return <Layout>loading...</Layout>;
+    }
+
+    if (!data?.post) {
+        return <Layout>Post not found</Layout>;
+    }
+
+    return (
+        <Layout variant='small'>
+            <Formik
+                initialValues={{ title: data.post.title, text: data.post.text }}
+                onSubmit={async (values) => {
+                    // await editPost({ variables: { id: intId, ...values } });
+                    router.back();
+                }}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <Box mt={4}>
+                            <InputField
+                                name='title'
+                                label='Title'
+                                placeholder='title'
+                            />
+                        </Box>
+                        <Box mt={4}>
+                            <InputField
+                                name='text'
+                                label='Body'
+                                placeholder='text...'
+                                textarea
+                            />
+                        </Box>
+
+                        <Button
+                            mt={4}
+                            type='submit'
+                            isLoading={isSubmitting}
+                            p={4}
+                            color='white'
+                            fontWeight='bold'
+                            borderRadius='md'
+                            bgGradient='linear(to-l, blue.600, pink.500)'
+                            _hover={{
+                                bgGradient: "linear(to-l, blue.500, pink.400)",
+                            }}>
+                            edit post
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Layout>
+    );
 };
 
-export default withUrqlClient(createUrqlClient)(EditPost);
+export default withUrqlClient(createUrqlClient, { ssr: true })(EditPost);
